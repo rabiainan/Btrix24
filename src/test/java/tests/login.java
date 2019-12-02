1a38e7a7f1a23fd193e09cec3375e71401a962d1
Rabia <r_inan@hotmail.com>
2019-12-02 20:23:06 +0000

    last uptade

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -3,6 +3,6 @@ package tests;
 public class login {
 
     public static void main(String[] args) {
-        System.out.println("user login");
+
     }
 }
--- src/test/java/tests/logout.java
+++ src/test/java/tests/logout.java
@@ -2,6 +2,8 @@ package tests;
 
 public class logout {
 
-
+    public static void main(String[] args) {
+        // this is something adding
+    }
 
 }
